6dd2344af12804f97a71d5d5c81c1a608e47b6a9
NeelimaSai95 <48699676+NeelimaSai95@users.noreply.github.com>
2026-02-09 00:12:32 -0600

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.stuller.com/", // <-- update
+    defaultCommandTimeout: 12000,
+    pageLoadTimeout: 60000,
+    retries: {
+      runMode: 2,
+      openMode: 0
+    },
+    env: {
+      username: "stullerqa24319", // set via CLI or cypress.env.json
+      password: "5p!sazyPBq"
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // keep empty unless you need plugins
+      return config;
     },
   },
 });
-
-module.exports = {
-  e2e: {
-    experimentalSessionAndOrigin: true
-  }
-}
